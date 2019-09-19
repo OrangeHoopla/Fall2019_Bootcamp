b66d2e3128874e89f22bbb994eca04c7afcbbc0a
quade <qkirby@ufl.edu>
2019-09-18 20:17:42 -0400

    HW: finished assignment

--- queries.js
+++ queries.js
@@ -1,5 +1,7 @@
 /* Add all the required libraries*/
-
+const mongoose = require('mongoose'),
+Listing = require('./ListingSchema.js'), 
+config = require('./config.js');
 /* Connect to your database using mongoose - remember to keep your key secret*/
 
 /* Fill out these functions using Mongoose queries*/
@@ -10,6 +12,11 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+Listing.findOne({
+    code: "LBW",
+  }, (error, listing) => {
+    console.log(listing);
+  })
 };
 var removeCable = function() {
   /*
@@ -17,6 +24,11 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+   Listing.findOneAndRemove({
+    code: "CABL",
+  }, (error, listing) => {
+    console.log(listing);
+  })
 };
 var updatePhelpsLab = function() {
   /*
@@ -26,11 +38,22 @@ var updatePhelpsLab = function() {
     Correct Address: 1953 Museum Rd, Gainesville, FL 32603
 
    */
+   Listing.findOneAndUpdate({
+    code: "PHL"
+  },
+  {
+    address: "1953 Museum Rd, Gainesville, FL 32603"
+  }, (error, listing) => {
+    console.log(listing)
+  })
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+   Listing.find((error, listings) => {
+    listings.forEach((listing) => console.log(listing));
+  });
 };
 
 findLibraryWest();
